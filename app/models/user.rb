e3bc074cc8073f42ec2d2bba0c7229800bd23e7d
Andrew Archer & Eddie <pair+amarcher.feek1g@devbootcamp.com>
2014-10-30 11:29:27 -0700

    added bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,18 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+  include BCrypt
+
   has_many :updates
   validates :email, :uniqueness => true
   validates :email, :format => {with: /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/}
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 end
