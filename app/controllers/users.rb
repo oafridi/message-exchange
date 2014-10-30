f14a88b7a05f31616a889b6993d38668f38524d1
Osman <osmanisalso@gmail.com>
2014-10-30 12:30:44 -0700

    added views for layout, profile, signup and header

---
+++ app/controllers/users.rb
@@ -0,0 +1,23 @@
+get '/' do
+  # if user is login go to profile page else login page
+  # if current_user
+  #   erb :profile
+  # else
+  #   erb :login
+  # end
+
+  erb :index
+end
+
+get '/signup' do
+  erb :signup
+end
+
+post '/profile' do
+  redirect '/profile'
+end
+
+get '/profile' do
+  @tweets = ["one", "two", "three"]
+  erb :profile
+end
