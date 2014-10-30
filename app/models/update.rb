56d08db0d76b5e5bf2321e280bbf42f0dfc62ac8
Andrew Archer & Eddie <pair+amarcher.feek1g@devbootcamp.com>
2014-10-30 12:26:11 -0700

    tests for update

--- app/models/update.rb
+++ app/models/update.rb
@@ -1,4 +1,4 @@
-class Updates < ActiveRecord::Base
+class Update < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :user
   validates :body, length: {minimum: 1, maximum: 140}
--- spec/update_spec.rb
+++ spec/update_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
 
 describe 'Update' do
-  pending "add some examples to (or delete) /Users/apprentice/Desktop/liltwitter/Rakefile"
+  before(:each) do
+    Update.delete_all
+  end
+
+  let(:user_params) { {     email: "test@gmail.com",
+                    password: "hello",
+                        name: "eddie" } }
+  let(:sample_user) { User.create(user_params) }
+  let(:valid_tweet_params) { {     body: "Hello my name is eddie.",
+                    user_id: sample_user.id } }
+  let(:not_valid_tweet_params) { {   body: "",
+                    user_id: sample_user.id } }
+
+  describe "Update.create" do
+    it "should add a Update to the database" do
+      expect {Update.create(valid_tweet_params)}.to change {Update.count}.by(1)
+    end
+
+    it "should prevent a Update without a name from being created" do
+      expect {Update.create(not_valid_tweet_params)}.to_not change {Update.count}.by(1)
+    end
+  end
+
+  describe Update do
+    it "should respond to body and user" do
+      [:body, :user].each do |method|
+        should respond_to method
+      end
+    end
+  end
 end
